00910534a9687d4c015c5d86c5fa2302a441441e
dnguyen2204 <31289956+dnguyen2204@users.noreply.github.com>
2018-10-08 11:52:11 -0700

    tasks 1-3

--- Main.java
+++ Main.java
@@ -25,22 +25,48 @@ class Main
         System.out.print(" Please type in an integer :");
         int n = s.nextInt();
         
+        
         /*
          * Task 1.  Create an array of integers from 0 to n-1
          *          call this array ar1.  Print it out below.
          */
+        
         System.out.println("*** PRINTING ar1 *");
+        int[] ar1 = new int[n];
+        for (int i=0; i<ar1.length; i++)
+        {
+            ar1[i] = i;
+        }
+        
+        
+        for (int i=0; i<ar1.length; i++)
+        {            
+            System.out.println("ar1[" + i + "]= " + ar1[i]);
+        }
+        
         
                 
         /*
          * Task 2.  Create a new array called ar2.
          *          Copy the elements of ar1 into ar2.
          */
+        int[] ar2 = new int[n];
+        for (int i=0; i<ar1.length; i++)
+        {
+            ar2[i] = ar1[i];
+            System.out.println(ar2[i]);
+        }
+        
         
         
         /* 
          * Task 3. Add 1 to each element in ar1.  Print it out below
          */
+        for (int i=0; i<ar1.length; i++)
+        {
+            ar1[i]= ar1[i]+1;
+            System.out.println("ar1[" + i + "]= " + ar1[i]);
+        }
         
 
         
