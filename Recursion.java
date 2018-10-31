4de04e26cd98a3ed91771e0ed6a6e3ea2c69411c
dnguyen2204 <31289956+dnguyen2204@users.noreply.github.com>
2018-10-31 11:24:59 -0700

    more recursion

--- Recursion.java
+++ Recursion.java
@@ -4,10 +4,13 @@ public class Recursion
     public static void main(String[] args)
     {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Type an integer");
+        
+        for (int i=0; i<5; i++)
+        {
+        System.out.println("Type a number");
         int x = sc.nextInt();
-        int answer = factorial2(x);
-        System.out.println(answer);
+        System.out.println("sumOdds(" + x +")= "+  sumOdds(x));
+        }
     }
     //definition of FACTORIAL    n*(n-1)*(n-2)*...*1 OR  
     //n!= n*(n-1)!
@@ -33,5 +36,53 @@ public class Recursion
             {
                 return n*factorial2(n-1);
             }
+    }
+    
+    public static int multiply(int n)//this is essentially the count function
+    {
+        if(n==0)
+        {
+                return 0;
+            }
+            else
+            {
+                return 12+multiply(n-1);
+                //multiplier+recursion
+            }
+            //return rooms*students, rooms=12, students=20
+            //recursion is the thing above, where n=students, and multiplier=rooms
+            //and recursion is calling itself
+            //definition of multiplication of n*x
+            //add n to itself x times
+    }
+    
+    public static int exponent(int n)
+    {
+        if(n==0)
+        {
+                return 1;
+            }
+            else
+            {
+                return 5*exponent(n-1);
+                //x is the multiplier here
+                //this function returns n^x
+            }
+        }
+        
+    public static int sumOdds(int n)//WRONG, TODO: FIX
+    {
+        if(n<=1)
+        {
+            return 1;
+        }
+        else if (n%2==1)
+        {
+            return n+sumOdds(n-2);
+        }
+        else //if even
+        {
+              return sumOdds(n-1);//does nothing except for make n an odd forever
+        }
     }
 }
