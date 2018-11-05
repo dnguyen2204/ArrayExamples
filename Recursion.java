2b94b8aa182151d4161b459e7551ce0e565d22db
dnguyen2204 <31289956+dnguyen2204@users.noreply.github.com>
2018-11-05 11:43:15 -0800

    SumEvens

--- Recursion.java
+++ Recursion.java
@@ -5,38 +5,12 @@ public class Recursion
     {
         Scanner sc = new Scanner(System.in);
         
-        for (int i=0; i<5; i++)
-        {
-        System.out.println("Type a number");
-        int x = sc.nextInt();
-        System.out.println("sumOdds(" + x +")= "+  sumOdds(x));
-        }
+        System.out.println(sumOdds(9));
+        System.out.println(sumEvens(8));
     }
     //definition of FACTORIAL    n*(n-1)*(n-2)*...*1 OR  
     //n!= n*(n-1)!
-    public static int factorial(int n)
-    {
-        //System.out.println(n);//prints
-        if(n>1)
-        {
-                return n*factorial(n-1);
-        }
-        return n;
-    }
-       
-    
-    //rephrased
-    public static int factorial2(int n)
-    {
-            if(n<=1)//stopping condition is important
-            {
-                    return n;
-            }
-            else//recursion
-            {
-                return n*factorial2(n-1);
-            }
-    }
+
     
     public static int multiply(int n)//this is essentially the count function
     {
@@ -55,24 +29,10 @@ public class Recursion
             //definition of multiplication of n*x
             //add n to itself x times
     }
-    
-    public static int exponent(int n)
-    {
-        if(n==0)
-        {
-                return 1;
-            }
-            else
-            {
-                return 5*exponent(n-1);
-                //x is the multiplier here
-                //this function returns n^x
-            }
-        }
         
     public static int sumOdds(int n)//GOOD, sumEvens would be switch the things
     {
-        if(n<=1)//n<=0
+        if(n<=1)//1 is lowest odd
         {
             return 1;//0
         }
@@ -80,9 +40,26 @@ public class Recursion
         {
             return n+sumOdds(n-2);
         }
-        else //if even//if odd
+        else //if even
         {
               return sumOdds(n-1);//does nothing except for make n an odd forever
         }
+    }
+    
+    public static int sumEvens(int n)
+    {
+        if (n<=2)//2 is lowest even
+        {
+                return 2;
+        }
+        else if (n%2==0)
+        {
+           return n+sumEvens(n-2);
+        }
+        else
+        {
+             return sumOdds(n-1);
+        }
+            
     }
 }
